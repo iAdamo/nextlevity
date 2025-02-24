c0ae03b86da0685100bf1bd02dbc6d77e293e86d
Adam_Babatunde_Sanusi <tundey520@gmail.com>
2025-02-24 22:05:54 +0100

    moved layout to component

--- screens/blog/index.tsx
+++ screens/blog/index.tsx
@@ -1,7 +1,7 @@
 import { SafeAreaView } from "@/components/ui/safe-area-view";
-import NavBar from "@/screens/layout/NavBar";
-import PreFooter from "@/screens/layout/PreFooter";
-import Footer from "@/screens/layout/Footer";
+import NavBar from "@/components/layout/NavBar";
+import PreFooter from "@/components/layout/PreFooter";
+import Footer from "@/components/layout/Footer";
 import BlogHeader from "./BlogHeader";
 import PopularArticles from "./PopularArticles";
 import RecentArticles from "./RecentArticles";
--- screens/homepage/index.tsx
+++ screens/homepage/index.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { SafeAreaView } from "@/components/ui/safe-area-view";
-import NavBar from "@/screens/layout/NavBar";
+import NavBar from "@/components/layout/NavBar";
+import PreFooter from "@/components/layout/PreFooter";
+import Footer from "@/components/layout/Footer";
 import HomeHeader from "@/screens/homepage/HomeHeader";
 import Categories from "@/screens/homepage/Categories";
 import ReviewsSection from "@/screens/homepage/ReviewsSection";
 import ReviewsSection2 from "@/screens/homepage/ReviewsSection2";
-
-import PreFooter from "@/screens/layout/PreFooter";
-import Footer from "@/screens/layout/Footer";
 import FaqSection from "@/screens/homepage/FaqSection";
 import MoreInfo from "@/screens/homepage/MoreInfo";
 import MarketingSolution from "./MaketingSolution";
