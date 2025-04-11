88c70091db4fe8a0f5a96c7a5011b2a0f8f76150
Adam_Babatunde_Sanusi <tundey520@gmail.com>
2025-04-11 17:31:26 +0100

    seo help

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextLevity",
+  title: "NextLevity | Innovative Digital Marketing Solutions",
   description:
-    "At Nextlevity, we specialize in turning your vision into reality. Our innovative strategies and creative solutions ensure your brand stands out in the digital landscape",
+    "At NextLevity, we specialize in turning your vision into reality. Our innovative strategies and creative solutions ensure your brand stands out in the digital landscape.",
+  keywords:
+    "digital marketing, business growth, branding, innovative solutions, NextLevity, next level, united kingdom, websites, programming",
+  authors: [{ name: "Adam Babatunde Sanusi, Michael Atunde" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "NextLevity | Innovative Digital Marketing Solutions",
+    description:
+      "Discover how NextLevity can help your business grow with cutting-edge digital marketing strategies and creative solutions.",
+    url: "https://www.nextlevity.co.uk",
+    type: "website",
+    images: [
+      {
+        url: "https://www.instagram.com/nextlevity.m/",
+        width: 1200,
+        height: 630,
+        alt: "NextLevity - Digital Marketing Solutions",
+      },
+    ],
+  },
 };
 
 import HomePage from "@/screens/homepage";
